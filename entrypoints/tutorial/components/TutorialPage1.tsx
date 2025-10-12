6b0b953834c35601fd6a3b0ce4cadcd3d074e0bc
Almenon <almenon214@gmail.com>
2025-10-11 21:57:06 -0700

    fix up styling

--- entrypoints/tutorial/components/TutorialPage1.tsx
+++ entrypoints/tutorial/components/TutorialPage1.tsx
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 import { WordStorageService } from '../../../lib/storage';
 import { scanAndReplaceWords } from '../../content/word-replacer';
 import AddWordForm from '../../options/components/AddWordForm';
+import './TutorialPage1.css';
 
 export default function TutorialPage1() {
     const [demoWords, setDemoWords] = useState<{ [key: string]: string }>({});
@@ -19,7 +20,7 @@ export default function TutorialPage1() {
             // Apply word replacement to the demo text element
             if (demoTextRef.current) {
                 // Reset the demo text to original
-                demoTextRef.current.innerHTML = "This is a sample text where you can see how the word replacer works. Try adding a word replacement below and watch this text change!";
+                demoTextRef.current.innerHTML = "The cat is sleeping on the table. I need to buy some food from the store. The weather is very nice today. My friend lives in a big house. We can go to the park tomorrow.";
 
                 // Get all word pairs from storage and apply them
                 const allWordPairs = await WordStorageService.getWordPairs();
@@ -33,22 +34,28 @@ export default function TutorialPage1() {
     const existingWords = Object.keys(demoWords);
 
     return (
-        <div>
-            <p>
-                #1: Add the English and Chinese words you want to practice.
-            </p>
+        <div className="tutorial-page">
+            <div className="tutorial-step">
+                <h2 className="step-header">
+                    <span className="step-number">1</span>
+                    <span className="step-text">Add the English and Chinese words you want to practice.</span>
+                </h2>
+            </div>
 
             <AddWordForm
                 onAddWord={handleAddWord}
                 existingWords={existingWords}
             />
 
-            <p>
-                #2: As you browse the web, your chosen words will appear in Chinese.
-            </p>
+            <div className="tutorial-step">
+                <h2 className="step-header">
+                    <span className="step-number">2</span>
+                    <span className="step-text">As you browse the web, your chosen words will appear in Chinese.</span>
+                </h2>
+            </div>
 
             <div className="demo-text" ref={demoTextRef}>
-                This is a sample text where you can see how the word replacer works. Try adding a word replacement below and watch this text change!
+                The cat is sleeping on the table. I need to buy some food from the store. The weather is very nice today. My friend lives in a big house. We can go to the park tomorrow.
             </div>
 
             {Object.keys(demoWords).length > 0 && (
