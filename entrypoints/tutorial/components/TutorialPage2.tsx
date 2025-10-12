1201852d64e3b9b483d2b97b500949102572f88c
Almenon <almenon214@gmail.com>
2025-10-12 09:52:40 -0700

    add extension icons

--- entrypoints/tutorial/components/TutorialPage2.tsx
+++ entrypoints/tutorial/components/TutorialPage2.tsx
@@ -1,10 +1,13 @@
+import extensions_button from '@/assets/extensions_button.png';
+import extension_icon from '@/public/wxt.svg';
+
 export default function TutorialPage2() {
     return (
         <div className="tutorial-page">
             <div className="tutorial-step">
                 <h2 className="step-header">
                     <span className="step-number">1</span>
-                    <span className="step-text">Click on the ~icon and then ~icon to adjust which words to replace</span>
+                    <span className="step-text">Click on the <img src={extensions_button}></img> and then <img src={extension_icon}></img> to adjust which words to replace</span>
                 </h2>
             </div>
 
