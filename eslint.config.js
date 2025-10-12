5564ce50386dccb588e7a9d9ae55ef875ea42206
Almenon <almenon214@gmail.com>
2025-10-12 09:39:19 -0700

    add react linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,26 @@
 import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-// import pluginReact from "eslint-plugin-react";
-import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  // todo implement react linting
-  //pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    rules: {
+      // Disable react-in-jsx-scope for React 17+ with new JSX transform
+      "react/react-in-jsx-scope": "off",
+      // reconfigure unescaped entities rule because it blocks " and ' by default
+      // and those are commonly used
+      "react/no-unescaped-entities": ["error", { "forbid": [">", "}"] }],
+    }
+  },
   globalIgnores([".output", ".wxt"])
 ]);
